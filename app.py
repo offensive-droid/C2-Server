177336d625e65d146064268aaa7bfa343d47d1f7
offensive-droid <fonderelite@gmail.com>
2024-11-13 01:41:25 +0800

    Added requirements.txt and credentials for DB are accessed through env file.

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
 from flask import Flask, render_template, request, redirect, url_for, session, send_from_directory, jsonify
 from psycopg2.extras import RealDictCursor
+from dotenv import load_dotenv
 import psycopg2,hashlib,os,base64
 
+# Load environment variables from .env file
+load_dotenv()
+
+DB_NAME = os.getenv("DB_NAME")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '1234'  # Replace with a strong, random secret key
