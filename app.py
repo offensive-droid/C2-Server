c35e3176b9096244930ea0b18390f798280c88a0
offensive-droid <138177031+offensive-droid@users.noreply.github.com>
2024-12-06 01:42:48 +0800

    Update app.py

--- app.py
+++ app.py
@@ -7,11 +7,6 @@ import psycopg2,hashlib,os,base64,secrets
 # Load environment variables from .env file
 load_dotenv()
 
-DB_NAME = os.getenv("DB_NAME")
-DB_USER = os.getenv("DB_USER")
-DB_PASSWORD = os.getenv("DB_PASSWORD")
-DB_HOST = os.getenv("DB_HOST")
-DB_PORT = os.getenv("DB_PORT")
 EXTERNAL_DB_URL = os.getenv("EXTERNAL_DB_URL")
 
 app = Flask(__name__)
